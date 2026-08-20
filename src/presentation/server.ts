875cd2a85b0f1ab39954474acc686dd0a83115bf
Max Diaz <mdiazn@uni.pe>
2026-08-20 15:42:12 -0500

    Servir SPA con router

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 export class Server {
   private port = 3000;
@@ -9,6 +10,12 @@ export class Server {
 
     // Public Folder
     this.app.use(express.static("public"));
+
+    this.app.get("/*splat", (req, res) => {
+      const indexPath = path.join(__dirname, "../../public/index.html");
+
+      res.sendFile(indexPath);
+    });
 
     this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port}`);
