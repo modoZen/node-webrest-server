a586488e62a21b86a65f6b745373be560b7099f0
Max Diaz <mdiazn@uni.pe>
2026-08-20 11:42:27 -0500

    Diferentes respuestas

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
+import { readFileSync } from "fs";
 import http from "http";
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
-  res.write("Hola mundo");
-  res.end();
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  // res.write(`<h1>URL: ${req.url}</h1>`);
+  // res.end();
+
+  // const data = { name: "John Doe", age: 30, city: "New York" };
+  // res.writeHead(200, { "Content-Type": "application/json" });
+  // res.end(JSON.stringify(data));
+
+  if (req.url === "/") {
+    const htmlFile = readFileSync("./public/index.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(htmlFile);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end();
+  }
 });
 
 server.listen(8080, () => {
