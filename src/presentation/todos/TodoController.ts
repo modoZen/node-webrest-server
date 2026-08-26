0d7c66961529ddceb3637c74ab7d757646b8f8aa
Max Diaz <mdiazn@uni.pe>
2026-08-26 16:37:43 -0500

    CRUD - Read

--- src/presentation/todos/TodoController.ts
+++ src/presentation/todos/TodoController.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from "express";
 
+const todos = [
+  { id: 1, text: "Buy milk", createdAt: new Date() },
+  { id: 2, text: "Buy bread", createdAt: null },
+  { id: 3, text: "Buy butter", createdAt: new Date() },
+];
+
 export class TodoController {
   //* DI
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
-    return res.json([
-      { id: 1, text: "Buy milk", createdAt: new Date() },
-      { id: 2, text: "Buy bread", createdAt: null },
-      { id: 3, text: "Buy butter", createdAt: new Date() },
-    ]);
+    return res.json(todos);
+  };
+
+  public getTodoById = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (isNaN(id))
+      return res.status(400).json({ error: "ID argument is not a number" });
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    todo
+      ? res.json(todo)
+      : res.status(400).json({ error: `TODO with id ${id} not found` });
   };
 }
--- src/presentation/todos/TodoRoutes.ts
+++ src/presentation/todos/TodoRoutes.ts
@@ -7,6 +7,7 @@ export class TodoRoutes {
     const todoController = new TodoController();
 
     router.get("/", todoController.getTodos);
+    router.get("/:id", todoController.getTodoById);
 
     return router;
   }
