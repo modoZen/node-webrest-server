a5b503cce30bba3a60663b852821d5c88d0874f0
Max Diaz <mdiazn@uni.pe>
2026-08-28 22:36:01 -0500

    CRUD - Delete

--- src/presentation/todos/TodoController.ts
+++ src/presentation/todos/TodoController.ts
@@ -62,4 +62,16 @@ export class TodoController {
 
     return res.json(todo);
   };
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo)
+      return res.status(404).json({ error: `TODO with id ${id} not found` });
+
+    todos.splice(todos.indexOf(todo), 1);
+
+    return res.json(todo);
+  };
 }
