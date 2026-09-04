9456ff7f0ad286d999355aae6d6fa932e89d8d87
Max Diaz <mdiazn@uni.pe>
2026-09-04 11:58:21 -0500

    Pruebas en app.ts

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,19 @@
+import { envs } from "../src/config/envs.js";
+import { Server } from "../src/presentation/server.js";
+
+vi.mock("../src/presentation/server.js");
+
 describe("Testing App", () => {
-  test("should work", () => {
-    expect(true).toBeTruthy();
+  test("should call server with arguments and start", async () => {
+    await import("../src/app.js");
+
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      PORT: envs.PORT,
+      PUBLIC_PATH: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    });
+
+    expect(Server.prototype.start).toHaveBeenCalledWith();
   });
 });
