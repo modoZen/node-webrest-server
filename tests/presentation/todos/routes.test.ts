946e35e7077add226f7372ac6eddeedfb01417c9
Max Diaz <mdiazn@uni.pe>
2026-09-04 18:23:40 -0500

    Pruebas GET /api/todos/:id

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -11,11 +11,14 @@ describe("Todo route testing", () => {
     testServer.close();
   });
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   const todo1 = { text: "Hola Mundo 1" };
   const todo2 = { text: "Hola Mundo 2" };
 
-  test("should return TODOs api/todos", async () => {
-    await prisma.todo.deleteMany();
+  it("should return TODOs api/todos", async () => {
     await prisma.todo.createMany({ data: [todo1, todo2] });
 
     const { body } = await request(testServer.app)
@@ -27,5 +30,19 @@ describe("Todo route testing", () => {
     expect(body[0].text).toBe(todo1.text);
     expect(body[1].text).toBe(todo2.text);
     expect(body[0].completedAt).toBeNull();
+  });
+
+  it("should return a TODO by id api/todos/:id", async () => {
+    const todo = await prisma.todo.create({ data: todo1 });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${todo.id}`)
+      .expect(200);
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo1.text,
+      completedAt: null,
+    });
   });
 });
